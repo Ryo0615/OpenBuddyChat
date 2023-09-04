aade98324fd22aa390343b0c85ed83e2d3bd95cb
Ryo Hirai <m1zuk1lac1e@gmail.com>
2023-09-04 23:36:21 +0900

    Update: ログ出力追加

--- app.py
+++ app.py
@@ -1,39 +1,52 @@
 import os
-
 import streamlit as st
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
 # Hugging FaceのAPIトークンを設定
-os.environ["HUGGINGFACE_TOKEN"] = os.getenv("HUGGINGFACE_TOKEN")
+token = os.getenv("HUGGINGFACE_TOKEN")
+if not token:
+    st.write("HUGGINGFACE_TOKEN not found!")
+else:
+    os.environ["HUGGINGFACE_TOKEN"] = token
 
 model_name_or_path = "TheBloke/OpenBuddy-Llama2-13B-v11.1-GGUF"
 model_basename = "openbuddy-llama2-13b-v11.1.Q2_K.gguf"
 
-model_path = hf_hub_download(
-    repo_id=model_name_or_path, filename=model_basename, revision="main"
-)
-llama = Llama(model_path)
+try:
+    model_path = hf_hub_download(
+        repo_id=model_name_or_path, filename=model_basename, revision="main"
+    )
+    st.write(f"Model downloaded to {model_path}")
+except Exception as e:
+    st.write(f"Error downloading model: {e}")
 
+try:
+    llama = Llama(model_path)
+except Exception as e:
+    st.write(f"Error initializing Llama: {e}")
 
 def predict(messages):
-    # Llamaでの回答を取得（ストリーミングオン）
-    streamer = llama.create_chat_completion(messages, stream=True)
+    try:
+        # Llamaでの回答を取得（ストリーミングオン）
+        streamer = llama.create_chat_completion(messages, stream=True)
 
-    partial_message = ""
-    for msg in streamer:
-        message = msg["choices"][0]["delta"]
-        if "content" in message:
-            partial_message += message["content"]
-            yield partial_message
-
+        partial_message = ""
+        for msg in streamer:
+            message = msg["choices"][0]["delta"]
+            if "content" in message:
+                partial_message += message["content"]
+                yield partial_message
+    except Exception as e:
+        st.write(f"Error in prediction: {e}")
 
 def main():
     st.title("Chat with ChatGPT Clone!")
 
     # Session state for retaining messages
-    if "messages" not in st.session_state:
+    if not hasattr(st.session_state, 'messages'):
         st.session_state.messages = []
+
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -69,7 +82,6 @@ def main():
         st.session_state.messages.append(
             {"role": "assistant", "content": full_response}
         )
-
 
 if __name__ == "__main__":
     main()
